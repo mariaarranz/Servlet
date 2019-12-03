5d2b84e70e53d8681273a5bc04f6b89cfb19caec
mariaarranz <maria_arranz@hotmail.es>
2019-12-03 13:37:52 +0000

    test and log added

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -3,11 +3,25 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 @WebServlet(urlPatterns={"/patients"},loadOnStartup = 1)
 public class MyServlet extends HttpServlet
 {
+    private static final Logger log= Logger.getLogger(MyServlet.class.getName());
+
+    public MyServlet()
+    {
+        log.info("In constructor");
+        log.warning("Running low on memory");
+        log.severe("Can't allocate memory");
+        
+        LogManager.getLogManager().readConfiguration(new FileInputStream("logging.properties"));
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
             IOException {
